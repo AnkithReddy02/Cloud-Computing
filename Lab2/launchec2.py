724fc541ea2f5f74428c1ceb0c80cc56c08f640b
Ankith Reddy <ankithreddy2001@gmail.com>
2021-09-08 23:35:55 +0530

    Update launchec2.py

--- Lab2/launchec2.py
+++ Lab2/launchec2.py
@@ -2,11 +2,9 @@ import boto3
 
 
 # aws configuration
-access_id_key = 'ASIA2QSCNKB22OK423HH'
-secret_access_key = 'PZNBAge3IlspHInQjhiDGKMN9quZeL4Tr/lLnrb/'
-session_token_key = 'FwoGZXIvYXdzEGYaDBRcQ71J7o9e3rfDSSLKAfg7CZukR6zQy/f8B3PUHau6rRk3TFcfS/4MX2XjNgcV49B1VXlledFH+E1NTZ9dJNWR45vYlTqbjV82P0Cimx7fh6dPXTx3XdOXFuyGSPXCXsfay5px8Y9atkO3hF+36EhhW+AyK9JJ3bnHLmVhIE40rOHhJPoI5nh4TnerLzTV0d2LhvKDR/vkZfZj4Vf7kBINjmEnS/J5PukAXakCcKrU+s+vzOmafEC+VSmNg8gKWONtBU7TYhOsjpWA7XeUEMwomSIctXLGYmsoju/giQYyLUcRIVSXLUK/MZ1kDcMW8uscHf4S2X69gvXYy852FJ+7iOeutbra6ziUE42/hA=='
-
-
+access_id_key = ''
+secret_access_key = ''
+session_token_key = ''
 
 ec2 = boto3.resource('ec2',aws_access_key_id = access_id_key,aws_secret_access_key = secret_access_key,aws_session_token = session_token_key,region_name='us-east-1')
 
