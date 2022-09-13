bae869498d4137f2f348ef3de6bccec2769b850d
Ankith Reddy <ankithreddy2001@gmail.com>
2022-09-13 18:34:53 +0530

    Delete Lab5/feedback_system_website directory

--- Lab5/feedback_system_website/connection.php
+++
@@ -1,9 +0,0 @@
-<?php include "../inc/dbinfo.inc"; ?>
-<?php
-   
-
-    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-    if(mysqli_connect_errno()) {
-        die("Failed to connect with MySQL: ". mysqli_connect_error());
-    }
-?>
--- Lab5/feedback_system_website/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ( !isset($_SESSION) ) session_start();
-print_r($_SESSION);
-session_destroy();
-header("Location: index.php");
-?>
